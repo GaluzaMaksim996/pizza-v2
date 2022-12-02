58cbec15f735bcecdb223c49e034618201694ef8
Maksim Galuza <krakket@bk.ru>
2022-12-02 13:14:23 +0300

    added cartSlice

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  items: [],
+  totalPrice: 0,
+};
+
+export const cartSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1,
+        });
+      }
+
+      state.totalPrice = calcTotalPrice(state.items);
+    },
+
+    minusItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+
+      if (findItem) {
+        findItem.count--;
+        state.totalPrice -= findItem.price;
+      }
+    },
+
+    removeItem(state, action) {
+      state.items = state.items.find((obj) => obj.id !== action.payload.id);
+    },
+
+    clearItems(state, action) {
+      state.items = [];
+      state.totalPrice = 0;
+    },
+  },
+});
+
+export const { addItem, minusItem, removeItem } = cartSlice.actions;
+
+export default cartSlice.reducer;
