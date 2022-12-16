3be04e69bd01dae32a7084931b304397de9fab61
Maksim Galuza <krakket@bk.ru>
2022-12-16 22:58:00 +0300

    add mainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
+import MainLayout from './layouts/MainLayout';
 import Cart from './pages/Cart';
 import FullPizza from './pages/FullPizza';
 import Home from './pages/Home';
@@ -10,17 +10,14 @@ import './scss/app.scss';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="pizza/:id" element={<FullPizza />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route element={<MainLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="pizza/:id" element={<FullPizza />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
